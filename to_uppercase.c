98a0dd4725221ad6f9d18ef62d6ef39a025a1966
a1940206 <1043684678@qq.com>
2025-10-12 17:59:29 +0800

    Update to_uppercase.c

--- to_uppercase.c
+++ to_uppercase.c
@@ -6,8 +6,7 @@ int main(void) {
   if (scanf("%20s", name) != 1) return 1;
 
   for (int i = 0; name[i] != '\0'; i++) {
-    if (name[i] >= 'a' && name[i] <= 'z')
-      name[i] -= 32;
+    if (name[i] >= 'a' && name[i] <= 'z') name[i] -= 32;
   }
 
   printf("%s\n", name);
