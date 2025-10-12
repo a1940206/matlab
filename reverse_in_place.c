24c97be5b49d78f45d4158061291da04257c6c06
a1940206 <1043684678@qq.com>
2025-10-12 17:56:47 +0800

    Update reverse_in_place.c

--- reverse_in_place.c
+++ reverse_in_place.c
@@ -2,17 +2,17 @@
 #include <string.h>
 
 int main(void) {
-    char str[21];
-    printf("Enter string: ");
-    scanf("%20s", str);
+  char str[21];
+  printf("Enter string: ");
+  if (scanf("%20s", str) != 1) return 1;
 
-    int len = strlen(str);
-    for (int i = 0; i < len / 2; i++) {
-        char temp = str[i];
-        str[i] = str[len - 1 - i];
-        str[len - 1 - i] = temp;
-    }
+  int len = strlen(str);
+  for (int i = 0; i < len / 2; i++) {
+    char temp = str[i];
+    str[i] = str[len - 1 - i];
+    str[len - 1 - i] = temp;
+  }
 
-    printf("%s\n", str);
-    return 0;
+  printf("%s\n", str);
+  return 0;
 }
