52b00ef17f8f5bfcf8d92ce52b7939523481e142
a1940206 <1043684678@qq.com>
2025-10-17 12:15:24 +0800

    Update odds_evens01.c

--- odds_evens01.c
+++ odds_evens01.c
@@ -17,12 +17,20 @@ int main(void) {
 
     printf("Enter %d integers: ", n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &nums[i]);
+        if (scanf("%d", &nums[i]) != 1) {
+            printf("Invalid input.\n");
+            free(nums);
+            return 1;
+        }
     }
+
     int odd_count = 0, even_count = 0;
     for (int i = 0; i < n; i++) {
-        if (nums[i] % 2 == 0) even_count++;
-        else odd_count++;
+        if (nums[i] % 2 == 0) {
+            even_count++;
+        } else {
+            odd_count++;
+        }
     }
 
     int *odds = malloc(odd_count * sizeof(int));
@@ -31,18 +39,26 @@ int main(void) {
     if (odds == NULL || evens == NULL) {
         printf("Memory allocation failed.\n");
         free(nums);
+        free(odds);
+        free(evens);
         return 1;
     }
+
     int oi = 0, ei = 0;
     for (int i = 0; i < n; i++) {
-        if (nums[i] % 2 == 0) evens[ei++] = nums[i];
-        else odds[oi++] = nums[i];
+        if (nums[i] % 2 == 0) {
+            evens[ei++] = nums[i];
+        } else {
+            odds[oi++] = nums[i];
+        }
     }
+
     for (int i = 0; i < odd_count; i++) {
         printf("%d", odds[i]);
         if (i < odd_count - 1) printf(" ");
     }
     printf("\n");
+
     for (int i = 0; i < even_count; i++) {
         printf("%d", evens[i]);
         if (i < even_count - 1) printf(" ");
@@ -54,3 +70,4 @@ int main(void) {
     free(evens);
     return 0;
 }
+
