714ec3ba716431c7ffa5f5e80e1f70596c5de949
a1940206 <1043684678@qq.com>
2025-10-17 12:20:11 +0800

    Refactor formatting and indentation in temperatures01.c

--- temperatures01.c
+++ temperatures01.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // for malloc
 
-int main(void) {
-    int count;
-    printf("Enter number of temperatures: ");
-    if (scanf("%d", &count) != 1 || count <= 0) {
-        printf("Invalid count.\n");
-        return 1;
+int main(void){
+  int count;
+  printf("Enter number of temperatures: ");
+  if (scanf("%d", &count) != 1 || count <= 0){
+    printf("Invalid count.\n");
+    return 1;
+  }
+
+  double *temps = malloc(count * sizeof(double));
+  if (temps == NULL){
+    printf("Memory allocation failed.\n");
+    return 1;
+  }
+
+  printf("Enter %d temperatures: ", count);
+  for (int i = 0; i < count; i++){
+    if (scanf("%lf", &temps[i]) != 1){
+      printf("Invalid input.\n");
+      free(temps);
+      return 1;
     }
+  }
 
-    double *temps = malloc(count * sizeof(double));
-    if (temps == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
+  for (int i = count - 1; i >= 0; i--){
+    printf("%.1f", temps[i]);
+    if (i > 0) printf(" ");
+  }
+  printf("\n");
 
-    printf("Enter %d temperatures: ", count);
-    for (int i = 0; i < count; i++) {
-        if (scanf("%lf", &temps[i]) != 1) {
-            printf("Invalid input.\n");
-            free(temps);
-            return 1;
-        }
-    }
-
-    for (int i = count - 1; i >= 0; i--) {
-        printf("%.1f", temps[i]);
-        if (i > 0) {
-            printf(" ");
-        }
-    }
-    printf("\n");
-
-    free(temps);
-    return 0;
+  free(temps);
+  return 0;
 }
